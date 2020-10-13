213ad29ffea97f6fea7a7a870716b034c917ae39
Agrael <Agrael@live.ru>
2020-10-13 19:49:02 +0200

    Bound Window and Screen events

---
+++ WinD3D12/App.h
@@ -0,0 +1,25 @@
+class App : public implements<App, IFrameworkViewSource, IFrameworkView>
+{
+public:
+    void Initialize(const CoreApplicationView& appView);
+    void SetWindow(const CoreWindow& window);
+    void Load(hstring const&);
+
+    void Uninitialize();
+    void Run();
+    IFrameworkView CreateView();
+public: //events
+    void OnActivated(const CoreApplicationView&, 
+        const IActivatedEventArgs& );
+    winrt::fire_and_forget OnSuspending(const IInspectable&,
+        const SuspendingEventArgs& args);
+    void OnResuming(const IInspectable&,const IInspectable&);
+    void OnWindowActivationChanged(const CoreWindow&, const WindowActivatedEventArgs& args);
+    void OnWindowSizeChanged(const CoreWindow& wnd, const WindowSizeChangedEventArgs& args);
+    void OnWindowClosed(const CoreWindow& wnd, const CoreWindowEventArgs& args);
+    void OnVisibilityChanged(const CoreWindow& wnd, const VisibilityChangedEventArgs& args);
+    void OnDpiChanged(const DisplayInformation& sender, const IInspectable& args);
+    void OnOrientationChanged(const DisplayInformation& sender, const IInspectable& args);
+    void OnStereoEnabledChanged(const DisplayInformation& sender, const IInspectable& args);
+    void OnDisplayContentsInvalidated(const DisplayInformation& sender, const IInspectable& args);
+};
--- WinD3D12/pch.h
+++ WinD3D12/pch.h
@@ -3,6 +3,19 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.Graphics.Display.h>
 #include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.UI.Composition.h>
 #include <winrt/Windows.UI.Input.h>
+
+using namespace winrt;
+using namespace Windows;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace winrt::Windows::Graphics::Display;
+using namespace Windows::Foundation::Numerics;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Composition;
+
